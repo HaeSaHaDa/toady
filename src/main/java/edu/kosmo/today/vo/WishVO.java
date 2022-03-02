4673ed4393f17c31d4061845597e61a6c101244e
boram-K <toto910811@naver.com>
2022-03-02 09:34:06 +0900

    commit wish

---
+++ src/main/java/edu/kosmo/today/vo/WishVO.java
@@ -0,0 +1,20 @@
+package edu.kosmo.today.vo;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Data;
+
+//03-02찜기능 구현 김보람
+
+/*
+WISHNUM NOT NULL NUMBER 
+MNUM             NUMBER 
+TNUM             NUMBER 
+ */
+@Data
+@Builder
+public class WishVO {
+
+	
+}
