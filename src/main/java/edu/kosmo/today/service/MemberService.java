951ca7e3500335d065dbb856edcb08f516d00f85
HaeSaHaDa <91727838+HaeSaHaDa@users.noreply.github.com>
2022-03-21 01:06:06 +0900

    Update MemberService.java

--- src/main/java/edu/kosmo/today/service/MemberService.java
+++ src/main/java/edu/kosmo/today/service/MemberService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import edu.kosmo.today.page.Criteria;
 import edu.kosmo.today.vo.NoteVO;
-import edu.kosmo.today.vo.UserVO;
+import edu.kosmo.today.vo.MemberVO;
 
 
 public interface MemberService {
@@ -14,6 +14,6 @@ public interface MemberService {
 		
 	public int getTotal();  //페이징 처리 함수
 	public List<UserVO> getListPage(Criteria criteria);
-	public void modify(UserVO memberVO); //권한 수정
-	public void nboardRegister(NoteVO NoteVO); //쪽지 작성 	
+	public void modify(MemberVO memberVO); //권한 수정
+	public void nboardRegister(MemberVO NoteVO); //쪽지 작성 	
 }
